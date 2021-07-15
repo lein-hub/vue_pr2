d1eff642ca60e32a4ddee9f510fd78ae44f6fb53
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-07-15 19:42:17 +0900

    practice

--- 3.숫자야구/NumberBaseball.vue
+++ 3.숫자야구/NumberBaseball.vue
@@ -1,28 +1,69 @@
 <template>
 <div>
     <h1>{{result}}</h1>
-    <form v-on:submit="onSubmitForm">
-        <input ref="answer" maxlength="4" v-model="value">
-        <button type="submit"></button>
+    <form @submit.prevent="onSubmitForm">
+        <input ref="answer" minlength="4" maxlength="4" v-model="value">
+        <button type="submit">입력</button>
     </form>
-    <div>시도:</div>
+    <div>시도: {{tries.length}}</div>
+    <ul>
+        <li v-for="t in tries">
+            <div>{{t.try}}</div>
+            <div>{{t.result}}</div>
+        </li>
+    </ul>
 </div>
     
 </template>
 
 <script>
+const getNumbers = () => {
+    const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [];
+    for (let i = 0; i < 4; i++) {
+        const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+
+    return array;
+}
+
 export default {
     data() {
         return {
+            answer: getNumbers(),
             tries: [],
             value: '',
             result: '',
         }
     },
     methods: {
-        onSubmitForm(e) {
-            e.preventDefault();
-            this.tries.push(e.target.value);
+        onSubmitForm() {
+            if (this.value === this.answer.join('')) {
+                this.tries.push({
+                    try: this.value,
+                    result: '홈런',
+                });
+                this.result = '홈런';
+                alert('게임을 다시 실행합니다');
+                this.tries = [];
+            } else {
+                let strike = 0;
+                let ball = 0;
+                const answerArray = this.value.split('').map(v => parseInt(v));
+                for (let i = 0; i < 4; i++) {
+                    if (answerArray[i] === this.answer[i]) { // 숫자, 자릿수 모두 정답
+                        strike++;
+                    } else if (this.answer.includes(answerArray[i])) { // 숫자만 정답
+                        ball++;
+                    }
+                }
+                this.tries.push({
+                    try: this.value,
+                    result: `${strike} 스트라이크 ${ball} 볼`,
+                });
+                
+            }
             this.$refs.answer.focus();
             this.value = '';
         }
