d08076fe1fedf0bfa7af5e02ca212e134fabba94
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-07-15 20:22:53 +0900

    practice

---
+++ 4.반응속도체크/ResponseCheck.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    <div id="screen">{{result}}</div>
+    <div>
+        <div>평균 시간: {{}}</div>
+        <button @click="onReset" :class="state"></button>
+    </div>
+</div>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            result: [],
+            state: 'waiting',
+            message: '클릭해서 시작하세요',
+        }
+    },
+    methods: {
+        onReset() {
+            
+        }
+    },
+};
+</script>
+
+<style>
+    #screen {
+        width: 300px;
+        height: 200px;
+        text-align: center;
+        user-select: none;
+    }
+
+    #screen.waiting {
+        background-color: aqua;
+    }
+
+    #screen.ready {
+        background-color: red;
+        color: white;
+    }
+
+    #screen.now {
+        background-color: greenyellow;
+    }
+</style>
