24491b691ac0a8eb6f440cfa653b56b72a346309
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-07-21 12:53:26 +0900

    vuex

---
+++ 7.틱택토Vuex/TicTacToe.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+    <p>{{turn}}님의 턴입니다.</p>
+    <table-component :table-data="tableData"></table-component>
+    <p v-if="winner">{{winner}}님의 승리!</p>
+</div>
+</template>
+
+<script>
+import store from './store';
+import TableComponent from './TableComponent';
+
+export default {
+    store,
+    components: { TableComponent },
+    data() {
+        return {
+            tableData: [
+                ['', '', ''], 
+                ['', '', ''], 
+                ['', '', ''],
+            ],
+            turn: 'O',
+            winner: '',
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+        
+    },
+    mounted() {
+    },
+    beforeDestroy() {
+    },
+};
+</script>
+
+<style>
+    table {
+        border-collapse: collapse;
+    }
+    td {
+        border: 1px solid black;
+        width: 40px;
+        height: 40px;
+        text-align: center;
+    }
+</style>
---
+++ 7.틱택토Vuex/store.js
@@ -0,0 +1,52 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESET_GAME = 'RESET_GAME';
+export const NO_WINNER = 'NO_WINNER';
+
+
+export default new Vuex.Store({
+    state: {
+        tableData: [
+            ['', '', ''], 
+            ['', '', ''], 
+            ['', '', ''],
+        ],
+        turn: 'O',
+        winner: '',
+    }, // vue의 data
+    getters: {
+
+    }, // vue의 computed
+    mutations: {
+        [SET_WINNER](state, winner) {
+            state.winner = winner;
+        },
+        [CLICK_CELL](state, {row, cell}) {
+            Vue.set(state.tableData[row], cell, state.turn);
+        },
+        [CHANGE_TURN](state) {
+            state.turn = state.turn === 'O' ? 'X' : 'O';
+        },
+        [RESET_GAME](state) {
+            state.turn = 'O';
+            state.tableData = [
+                ['', '', ''], 
+                ['', '', ''], 
+                ['', '', ''],
+            ];
+        },
+        [NO_WINNER](state) {
+            state.winner = '';
+
+        },
+    }, // state를 수정할 때 사용한다. 동기적
+    actions: {
+
+    }, // 비동기를 사용할 때, 또는 여러 뮤테이션을 연달아 실행할 때
+});
